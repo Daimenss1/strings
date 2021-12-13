2132c6e7a746e272c38e6fc6b3cfca4885e41590
Саша <saniamit@mail.ru>
2021-12-13 23:38:08 +0300

    ДЗ по теме строки

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,25 @@
 package ru.skypro;
 
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastname = "Ivanov";
+        String fullName = (firstName + " " + middleName + " " + lastname);
+        System.out.println(" * Задача 1 ");
+        System.out.println(" ФИО сотрудника — " + fullName);
+                System.out.println(" * Задача 2 ");
+        System.out.println(fullName.toUpperCase(Locale.ROOT));
+        System.out.println(" * Задача 3 ");
+        String result = fullName.replace(" ", ";");
+        System.out.println(" Данные ФИО сотрудника для административного отдела - " + result);
+
+        String fullName2 = " Иванов Семён Семёнович";
+        String chooseName = fullName2.replace(" ё", " е");
+        System.out.println(" * Задача 4 ");
+        System.out.println(" Данные ФИО сотрудника - " + chooseName);
     }
 }
